e5e74fed30526682fd21fa2446a6a5a47dbe0934
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2014-08-10 14:49:08 +0100

    New styling, and GA tracking added

---
+++ MelbournePhysiotherapy/Controllers/TestimonialsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MelbournePhysiotherapy.Controllers
+{
+    public class TestimonialsController : Controller
+    {
+        //
+        // GET: /Testimonials/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
